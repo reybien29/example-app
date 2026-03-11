eb87d5428903b393d19e47cd77b740de15cd9258
Rey Bien <caneja.alum@gmail.com>
2026-03-11 23:09:38 +0800

    fix: use verified 44-char APP_KEY (previous one was malformed 45-char)

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => 'base64:7U5pDLvPzJK2Y13ra1FdvqrNNgk+3ixABuwx1sys658E=',
+    'key' => env('LARAVEL_KEY', env('APP_KEY', 'base64:l5ayUZRCzEdPajkNIF0BWAuLCdXNfaHSWcAbmvWBdHo=')),
 
     'previous_keys' => [
         ...array_filter(
